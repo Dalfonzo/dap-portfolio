be0cc7356e024b677b38415ce52220fcbdbcb330
David Pérez <dalfonzo18@gmail.com>
2022-07-30 18:57:31 -0400

    Adds responsive ScrollTrigger

--- src/static/js/projects.js
+++ src/static/js/projects.js
@@ -4,41 +4,9 @@ let panelsContainer = document.querySelector("#panels-container");
 
 /* Panels */
 const panels = gsap.utils.toArray("#panels-container .panel-horizontal");
-// gsap.to(panels, {
-//   xPercent: -100 * (panels.length - 1),
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: "#panels-container",
-//     pin: true,
-//     start: "top top",
-//     scrub: 1,
-//     snap: {
-//       snapTo: 1 / (panels.length - 1),
-//       inertia: false,
-//       duration: { min: 0.1, max: 0.1 },
-//     },
-//     end: () => "+=" + (panelsContainer.offsetWidth - innerWidth),
-//   },
-// });
 
-let isAlreadyEnabled = false;
-WidthChange(window.innerWidth >= 900);
-
-window.addEventListener("resize", () => {
-  WidthChange(window.innerWidth >= 900);
-});
-
-function WidthChange(shouldEnable) {
-  const trigger = ScrollTrigger.getById("trigger");
-
-  if (!shouldEnable && trigger) {
-    gsap.killTweensOf([trigger.scroller, trigger.animation]);
-    gsap.set(panels, { xPercent: 0, overwrite: true, height: "100px" });
-    trigger.disable(true, false);
-    isAlreadyEnabled = false;
-  }
-
-  if (shouldEnable && !isAlreadyEnabled) {
+ScrollTrigger.matchMedia({
+  "(min-width: 800px)": function () {
     gsap.to(panels, {
       xPercent: -100 * (panels.length - 1),
       ease: "none",
@@ -57,16 +25,25 @@ function WidthChange(shouldEnable) {
       },
     });
 
-    isAlreadyEnabled = true;
-  }
-}
-
-gsap.utils.toArray(".panel-vertical").forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    id: "trigger2",
-    start: "top top",
-    pin: true,
-    pinSpacing: false,
-  });
+    gsap.utils.toArray(".panel-vertical").forEach((panel, i) => {
+      ScrollTrigger.create({
+        trigger: panel,
+        id: "trigger2",
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+      });
+    });
+  },
+  "(max-width: 799px)": function () {
+    gsap.utils.toArray(".panel-vertical").forEach((panel, i) => {
+      ScrollTrigger.create({
+        trigger: panel,
+        id: "trigger2",
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+      });
+    });
+  },
 });
